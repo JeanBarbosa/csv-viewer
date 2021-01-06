10241bf43aa2d6095cb60fc2424ec90b98b391e0
Jean Barbosa <programmer.jean@gmail.com>
2021-01-06 13:52:51 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const { BrowserWindow, app } = require('electron');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile('src/index.html');
+
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+})
