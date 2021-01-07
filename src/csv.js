b408114feb0114d3e6a5c665adb653885f4e5486
Jean Barbosa <programmer.jean@gmail.com>
2021-01-07 19:43:03 -0300

    feat: add action drag and drop file

--- src/csv.js
+++ src/csv.js
@@ -5,20 +5,44 @@ const { ipcRenderer } = require('electron');
 const selectDirBtn = document.getElementById('select-directory');
 const modal = require('./modal');
 
-selectDirBtn.addEventListener('click', (event) => {
-  ipcRenderer.send('open-file-dialog');
-});
-
-ipcRenderer.on('selected-directory', (event, path) => {
-  // document.getElementById('selected-file').innerHTML = `You selected: ${path}`;
-
-  if (Array.isArray(path)) {
-    console.log(path[0])
-    fs.createReadStream(path[0])
+const getFile = (filePath) => {
+  if (Array.isArray(filePath)) {
+    fs.createReadStream(filePath[0])
+      .pipe(csv())
+      .on('data', (data) => results.push(data))
+      .on('end', () => {
+        modal(results);
+      });
+  } else {
+    fs.createReadStream(filePath)
       .pipe(csv())
       .on('data', (data) => results.push(data))
       .on('end', () => {
         modal(results);
       });
   }
+}
+
+selectDirBtn.addEventListener('click', (event) => {
+  ipcRenderer.send('open-file-dialog');
+});
+
+document.addEventListener('drop', (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+
+  for (const f of event.dataTransfer.files) {
+    if (f.type === "text/csv")
+      getFile(f.path);
+  }
+});
+
+document.addEventListener('dragover', (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+});
+
+ipcRenderer.on('selected-directory', (event, path) => {
+  // document.getElementById('selected-file').innerHTML = `You selected: ${path}`;
+  getFile(path);
 });
